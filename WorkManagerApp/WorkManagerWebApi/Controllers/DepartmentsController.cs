33c95835d4b841bd53a338f6a9778c89fc339036
Vladislav Kurbatov <gromazekazeka@gmail.com>
2024-02-16 19:34:33 +0300

    Добавьте файлы проекта.

---
+++ WorkManagerApp/WorkManagerWebApi/Controllers/DepartmentsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorkManagerWebApi.Controllers
+{
+    [Route("api/departments")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+    }
+}
---
+++ WorkManagerApp/WorkManagerWebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorkManagerWebApi.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+    }
+}
